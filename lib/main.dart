d907ce3bc01ccc318237a2841844589eed39cb7f
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-04-26 17:03:33 +0900

    fix : uid 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:alimipro_mock_data/manage/data/data_source/academy_data_source.dart';
 import 'package:alimipro_mock_data/manage/data/data_source/log_data_source.dart';
 import 'package:alimipro_mock_data/manage/presentation/academy_student_list_screen.dart';
-import 'package:alimipro_mock_data/manage/presentation/student_list_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -42,7 +41,7 @@ class MyApp extends StatelessWidget {
           academyDataSource: AcademyDataSource(firebaseFirestore: db),
           logDataSource: LogDataSource(firebaseFirestore: db),
         ),
-        uid: '2rkvZ4XPwqfVdIBmmBVPRm4sVcB3',
+        uid: '',
       ),
     );
   }
