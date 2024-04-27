331e30ce5cb27a98187d9e1966c0dad5d1859641
JEON-Sungsu <anagma@naver.com>
2024-04-27 23:25:18 +0900

    feat: 등하원 기록 ViewModel, useCase 작성

---
+++ lib/manage/domain/use_case/get_personal_punchlogs_use_case.dart
@@ -0,0 +1,20 @@
+import 'package:alimipro_mock_data/manage/domain/model/personal_punch_log.dart';
+import 'package:alimipro_mock_data/manage/domain/repository/log_data_repository.dart';
+
+class GetPersonalPunchLogsUseCase {
+  final LogDataRepository _logDataRepository;
+
+  Future<List<PersonalPunchLog>> getPersonalPunchLogs(
+    String name,
+    String parentPhone,
+    int pastFromToday,
+  ) async {
+    final punchLogs = await _logDataRepository.getStudentsPunchLogs(name, parentPhone, pastFromToday);
+
+    return punchLogs;
+  }
+
+  const GetPersonalPunchLogsUseCase({
+    required LogDataRepository logDataRepository,
+  }) : _logDataRepository = logDataRepository;
+}
---
+++ lib/manage/presentation/view_model/student_punch_log_view_model.dart
@@ -0,0 +1,24 @@
+import 'package:alimipro_mock_data/manage/domain/model/personal_punch_log.dart';
+import 'package:alimipro_mock_data/manage/domain/use_case/get_personal_punchlogs_use_case.dart';
+import 'package:flutter/material.dart';
+
+class StudentPunchLogViewModel with ChangeNotifier {
+  List<PersonalPunchLog> _punchLogs = [];
+  final GetPersonalPunchLogsUseCase _personalPunchLogsUseCase;
+
+  List<PersonalPunchLog> get punchLogs => List.unmodifiable(_punchLogs);
+
+  Future<void> setPunchLogs(
+    String name,
+    String parentPhone,
+    int pastFromToday,
+  ) async {
+    _punchLogs = await _personalPunchLogsUseCase.getPersonalPunchLogs(
+        name, parentPhone, pastFromToday);
+    notifyListeners();
+  }
+
+  StudentPunchLogViewModel({
+    required GetPersonalPunchLogsUseCase personalPunchLogsUseCase,
+  }) : _personalPunchLogsUseCase = personalPunchLogsUseCase;
+}
