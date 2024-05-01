97845b87e0dcc2289189f3465087c575d5ac76fe
leeseowoo <leeseowoo.kr@gmail.com>
2024-05-02 03:43:40 +0900

    chore: use auth emulator

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:alimipro_mock_data/core/router/router.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,7 @@ void main() async {
   );
 
   final db = FirebaseFirestore.instance..useFirestoreEmulator('10.0.2.2', 8080);
+  FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
 
   runApp(MyApp(db: db));
 }
