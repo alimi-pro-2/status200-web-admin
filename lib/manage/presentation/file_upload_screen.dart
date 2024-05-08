2a0948c39474cd8a9a0e2e73367f57d0d7a66f25
sangjun <byjun8932@gmail.com>
2024-05-08 15:08:57 +0900

    feat: 최종 버그 해결 , 주석 및 unused import 제거

--- lib/manage/presentation/file_upload_screen.dart
+++ lib/manage/presentation/file_upload_screen.dart
@@ -6,7 +6,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 
 import 'component/input_dialog.dart';
-import 'component/size_exceed_dialog.dart';
 import 'component/submit_success_dialog.dart';
 import 'component/upload_success_dialog.dart';
 
@@ -33,6 +32,8 @@ class _FileUploadScreenState extends State<FileUploadScreen> {
     result = await FilePicker.platform.pickFiles();
     if (result!.files.first.size > 3 * 1024 * 1024) {
       limitexdFile = true;
+      const String ErrorMessage = '3M가 이상파일은 첨부할 수 없습니다.';
+      await showInputDialog(ErrorMessage, context);
     } else {
       limitexdFile = false;
     }
@@ -58,36 +59,29 @@ class _FileUploadScreenState extends State<FileUploadScreen> {
 
       // 업로드된 파일의 다운로드 URL 가져오기
       downloadUrl = await snapshot.ref.getDownloadURL();
-
-      Map<String, dynamic> noticeMap = {
-        'academy': widget.academyInfo['name'], // 학원 이름
-        'contents': contents, // 공지 내용
-        'date': Timestamp.now(), // 현재 시간을 Timestamp로 저장
-        'parentsPhone': parentPhone, // 학부모 전화번호
-        'pushType': 'notice', // 푸시 타입 (예: SMS, 앱 푸시 등)
-        'title': title, // 공지 제목
-        'fileUrl': downloadUrl, // 업로드된 파일의 다운로드 URL
-        'filename': filename,
-      };
-
-      print(downloadUrl);
-
-      String id = await noticeViewModel.setNotice(noticeMap);
-      await noticeViewModel.getNotice(id);
-      print(id);
-      print(noticeViewModel.notice.toString());
-
-      downloadFin = true;
-      // 파일 업로드 및 데이터 저장 완료
-      print('File uploaded and notice added to Firestore!');
-      await showUploadSuccessDialog(context);
-    } else {
-      // 파일 크기가 3MB 초과
-      await showSizeExceedDialog(context);
-      print('File size exceeds 3MB. Please choose a smaller file.');
-
-      downloadFin = false;
     }
+    Map<String, dynamic> noticeMap = {
+      'academy': widget.academyInfo['name'], // 학원 이름
+      'contents': contents, // 공지 내용
+      'date': Timestamp.now(), // 현재 시간을 Timestamp로 저장
+      'parentsPhone': parentPhone, // 학부모 전화번호
+      'pushType': 'notice', // 푸시 타입 (예: SMS, 앱 푸시 등)
+      'title': title, // 공지 제목
+      'fileUrl': downloadUrl, // 업로드된 파일의 다운로드 URL
+      'filename': filename,
+    };
+
+    print(downloadUrl);
+
+    String id = await noticeViewModel.setNotice(noticeMap);
+    await noticeViewModel.getNotice(id);
+    print(id);
+    print(noticeViewModel.notice.toString());
+
+    downloadFin = true;
+    // 파일 업로드 및 데이터 저장 완료
+    print('Fire Store 에 파일이 저장됐어요!!!!');
+    await showUploadSuccessDialog(context);
   }
 
   @override
@@ -132,14 +126,17 @@ class _FileUploadScreenState extends State<FileUploadScreen> {
                   } else if (parentsPhone == '') {
                     err = '전화번호';
                   }
-                  String ErrorMessage = '$err이 입력되지 않았어요';
-                  if (limitexdFile) {
-                    ErrorMessage = '3M가 이상파일은 첨부할 수 없습니다.';
-                  }
+                  String ErrorMessage = '$err이(가) 입력되지 않았어요';
+
                   downloadFin = false;
                   await showInputDialog(ErrorMessage, context);
                 } else {
                   downloadFin = true;
+                }
+
+                if (limitexdFile) {
+                  const String ErrorMessage = '3mb가 이상파일은 첨부할 수 없습니다.';
+                  await showInputDialog(ErrorMessage, context);
                 }
 
                 if (downloadFin && !limitexdFile) {
